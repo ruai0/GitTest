aeb6e8933d5f5bb2bed16f101e6bccb014712145
ruai0”0git config --global user.name ruai0” <11515886+jiang-zetong@user.noreply.gitee.com~>
2024-08-08 21:28:15 +0800

    第一次idea提交

---
+++ src/main/java/cn/lanqiao/Hello1.java
@@ -0,0 +1,14 @@
+package cn.lanqiao;
+
+import java.math.BigDecimal;
+
+public class Hello1 {
+    public static void main(String[] args) {
+        String s1 ="100";
+        String s2 ="30";
+        BigDecimal price = new BigDecimal(s1);
+        BigDecimal price2 = new BigDecimal(s2);
+        double result = price.divide(price2,2,BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println(result);
+    }
+}
